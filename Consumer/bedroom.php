02d5f6731171076bae6b4f8c5ac189b11081fe87
Büşra Sarıgeyik <79912293+Busraa8@users.noreply.github.com>
2023-06-14 02:22:19 +0300

    thermostat and light

--- Consumer/bedroom.php
+++ Consumer/bedroom.php
@@ -81,6 +81,61 @@ if ($result->num_rows > 0) {
 } else {
 }
 
+// Light
+// Veritabanından değeri çek
+$sqlBrightness = "SELECT JSON_EXTRACT(properties, '$.brightness') AS brightness FROM devices WHERE room_id = 1 AND device_name = 'Light'";
+$resultBrightness = $conn->query($sqlBrightness);
+
+if ($resultBrightness->num_rows > 0) {
+    $rowBrightness = $resultBrightness->fetch_assoc();
+    $brightness = $rowBrightness["brightness"];
+} else {
+    $brightness = 0; 
+}
+
+$newBrightness = 50; 
+
+$stmtBrightness = $conn->prepare("UPDATE devices SET properties_consumer = JSON_SET(properties_consumer, '$.brightness', ?) WHERE room_id = 1 AND device_name = 'Light'");
+$stmtBrightness->bind_param("i", $newBrightness);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $newBrightness = $_POST['brightness'];
+
+    if (!$stmtBrightness->execute()) {
+        echo "Hata (Brightness): " . $stmtBrightness->error;
+    }
+}
+
+$stmtBrightness->close();
+
+// Temperature
+// Veritabanından değeri çek
+$sqlTemperature = "SELECT JSON_UNQUOTE(JSON_EXTRACT(properties, '$.temperature')) AS temperature FROM devices WHERE room_id = 1 AND device_name = 'Thermostat' AND JSON_UNQUOTE(JSON_EXTRACT(properties, '$.temperature')) = 25";
+$resultTemperature = $conn->query($sqlTemperature);
+
+if ($resultTemperature->num_rows > 0) {
+    $rowTemperature = $resultTemperature->fetch_assoc();
+    $temperature = $rowTemperature["temperature"];
+} else {
+    $temperature = 0; 
+}
+
+$newTemperature = 30;
+
+$stmtTemperature = $conn->prepare("UPDATE devices SET properties_consumer = JSON_SET(properties_consumer, '$.temperature', ?) WHERE room_id = 1 AND device_name = 'Thermostat' AND JSON_UNQUOTE(JSON_EXTRACT(properties, '$.temperature')) = 25");
+$stmtTemperature->bind_param("i", $newTemperature);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $newTemperature = $_POST['temperature'];
+
+    if (!$stmtTemperature->execute()) {
+        echo "Hata (Temperature): " . $stmtTemperature->error;
+    }
+}
+
+$stmtTemperature->close();
+
+
 // Veritabanı bağlantısını kapat
 $conn->close();
 ?>
@@ -167,37 +222,36 @@ $conn->close();
       </div>
 
       <div class="containert">
-        <p style="font-size: 15px; color: whitesmoke;">Temperature</p>
+                  <p style="font-size: 15px; color: whitesmoke;">Thermostat</p>
+                  <div class="status-panel">
+                    <div class="status-card">
+                      <span id="living-room-temp-value-degree"><?php echo $temperature; ?></span>
+                      <h3 style="text-align: center; color: whitesmoke;">Thermostat</h3><br/><br/>
+                      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                      <input type="range" id="living-room-temp-input" name="temperature" min="10" max="38" step="1" style="color: aliceblue" value="<?php echo $temperature; ?>" oninput="document.getElementById('living-room-temp-value').innerHTML = this.value;">
+                      <span id="living-room-temp-value"><?php echo $temperature; ?></span> 
+                      <button type="submit" class="header_pro_devices">Update</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
 
-        <div class="status-panel">
-          <div class="status-card">
-
-            <span id="living-room-temp">25&deg;C</span>
-            <h3 style="text-align: center;color: whitesmoke;">Temperature</h3><br /><br /><br /><br />
-            <input type="range" id="living-room-temp-input" min="10" max="35" step="1" style="color:aliceblue">
-
-          </div>
-
-        </div>
-
-      </div>
-
-      <div class="containert">
-        <p style="font-size: 15px; color: whitesmoke;">Light</p>
-
-        <div class="status-panel">
-          <div class="status-card">
-
-            <span id="living-room-light-value-degree">50</span>
-            <h3 style="text-align: center;color: whitesmoke;">Light</h3><br /><br /><br /><br />
-            <input type="range" id="living-room-light-input" min="0" max="100" step="1" style="color:aliceblue">
-            <span id="living-room-light-value">50</span>
-
-          </div>
-
-        </div>
-
-      </div>
+    
+                <div class="containert">
+                  <p style="font-size: 15px; color: whitesmoke;">Light</p>
+                  <div class="status-panel">
+                    <div class="status-card">
+                      <span id="living-room-light-value-degree"><?php echo $brightness; ?></span>
+                      <h3 style="text-align: center; color: whitesmoke;">Light</h3><br/><br/>
+                      
+                      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                      <input type="range" id="living-room-light-input" name="brightness" min="0" max="100" step="1" style="color: aliceblue" value="<?php echo $brightness; ?>" oninput="document.getElementById('living-room-light-value').innerHTML = this.value;">
+                      <span id="living-room-light-value"><?php echo $brightness; ?></span> 
+                      <button type="submit" class="header_pro_devices">Update</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
 
 
       <form method="post" action="bedroom.php">
