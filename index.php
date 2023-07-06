34ec22c3b0d5d8924faaa39b0d2a3bc2b9856bfb
Büşra Sarıgeyik <79912293+Busraa8@users.noreply.github.com>
2023-07-06 13:58:13 +0300

    validation

--- index.php
+++ index.php
@@ -4,31 +4,6 @@
 <head>
     <title>Login</title>
     <link rel="stylesheet" href="Consumer/login.css">
-    <script>
-        function validateForm() {
-            var email = document.getElementById("email").value;
-            var password = document.getElementById("password").value;
-
-            if (email === "") {
-                alert("Email alanı boş olamaz.");
-                return false;
-            }
-
-            // E-posta formatını kontrol etme
-            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (!emailRegex.test(email)) {
-                alert("Geçerli bir e-posta adresi giriniz.");
-                return false;
-            }
-
-            if (password === "") {
-                alert("Şifre alanı boş olamaz.");
-                return false;
-            }
-
-            return true;
-        }
-    </script>
 </head>
 <body>
     <div id="container">
@@ -37,12 +12,12 @@
         </div>
         <div id="right">
             <h1 id="login">Consumer LogIn</h1><br>
-            <form action="" method="post" onsubmit="return validateForm()"> 
+            <form action="" method="post"> 
                 <input type="email" id="email" name="email" class="client-info" required>
                 <label for="email">Email</label>
                 <input type="password" id="password" name="password" class="client-info" required>
                 <label for="password">Password</label>
-                <input type="submit" id="submit" name="login" class="client-info" value="LogIn" >
+                <input type="submit" id="submit" name="login" class="client-info" value="LogIn">
             </form>
             <a href="select.php" style="text-decoration: none;">
                 <button class="social" id="forgot">change user role</button>
